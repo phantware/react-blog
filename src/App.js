cfe61d49e108a9b1371df77a4d805cb51c1dc33a
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-01-26 22:30:26 +0100

    Header and Home page Implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react'
+import Home from './pages/home/Home'
 import Topbar from './topbar/Topbar'
 
 function App() {
   return (
     <div>
       <Topbar />
+      <Home />
     </div>
   )
 }
