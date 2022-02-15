979da2061b6db025076bff14d9b012868d20ee89
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-02-15 20:14:18 +0100

    useffect added to home page

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../../header/Header'
 import Posts from '../../posts/Posts'
 import Sidebar from '../../sidebar/Sidebar'
 import './home.css'
+import axios from 'axios'
 
 const Home = () => {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get('/posts')
+      setPosts(res.data)
+    }
+    fetchPosts()
+  }, [])
   return (
     <>
       <Header />
       <div className='home'>
-        <Posts />
+        <Posts posts={posts} />
         <Sidebar />
       </div>
     </>
