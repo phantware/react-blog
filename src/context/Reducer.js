85b1801b854320cf963da8b3fb831bd2722da8bf
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-02-19 18:39:57 +0100

    Reducer logic created

---
+++ src/context/Reducer.js
@@ -0,0 +1,29 @@
+const Reducer = (action, state) => {
+  switch (action.type) {
+    case 'LOGIN_START': {
+      return {
+        user: null,
+        isFetching: true,
+        error: false,
+      }
+    }
+    case 'LOGIN_SUCCESS': {
+      return {
+        user: action.payload,
+        isFetching: false,
+        error: false,
+      }
+    }
+    case 'LOGIN_ERROR': {
+      return {
+        user: null,
+        isFetching: false,
+        error: true,
+      }
+    }
+    default:
+      return state
+  }
+}
+
+export default Reducer
