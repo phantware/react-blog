ca5f9d1966b58d220ac1b53e0c9df199fb56f1e7
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-02-19 18:59:45 +0100

    contexprovider wrapped over appjs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { ContexProvider } from './context/Context'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ContexProvider>
+      <App />
+    </ContexProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
