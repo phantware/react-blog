d9064cd4fa5c90efbbf84a8ba35477fd0e8788d8
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-02-19 08:25:13 +0100

    Link to singlePost by user name created

--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { useLocation } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import './singlePost.css'
 import axios from 'axios'
 
@@ -31,7 +31,10 @@ const SinglePost = () => {
         </h1>
         <div className='singlePostInfo'>
           <span className='singlePostAuthor'>
-            Author : <b>{post.username}</b>
+            Author :
+            <Link to={`/?user=${post.username}`} className='link'>
+              <b>{post.username}</b>
+            </Link>
           </span>
           <span className='singlePostDate'>
             {new Date(post.createdAt).toDateString()}
