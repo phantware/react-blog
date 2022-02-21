2f8639060106de5db6c7dd251484821b9f0ed8f6
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-02-21 06:53:15 +0100

    login button disabled for isFetching and styled in css

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -8,7 +8,7 @@ import './login.css'
 const Login = () => {
   const userRef = useRef()
   const passwordRef = useRef()
-  const { user, dispatch, isFetching } = useContext(Context)
+  const { dispatch, isFetching } = useContext(Context)
 
   const handleSubmit = async (e) => {
     e.preventDefault()
@@ -23,7 +23,7 @@ const Login = () => {
       dispatch({ tpe: 'LOGIN_ERROR' })
     }
   }
-  console.log(user)
+
   return (
     <div className='login'>
       <span className='loginTitle'>Login</span>
@@ -42,7 +42,7 @@ const Login = () => {
           placeholder='Enter your password'
           ref={passwordRef}
         />
-        <button className='loginButton' type='submit'>
+        <button className='loginButton' type='submit' disabled={isFetching}>
           Login
         </button>
       </form>
